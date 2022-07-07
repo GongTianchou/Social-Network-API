6f8c87348f332ba115e62615d6103d3fc053b9ce
Tianchou Gong <tianchougong@gmail.com>
2022-07-07 16:39:48 -0400

    getuser by id and create user added

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -14,4 +14,34 @@ const userController = {
           res.status(400).json(err);
         });
     },
+
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+          .populate({
+            path: "thoughts",
+            select: "-__v",
+          })
+          .populate({
+            path: "friends",
+            select: "-__v",
+          })
+          .select("-__v")
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No user found with this ID" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      },
     
+      createUser({ body }, res) {
+        User.create(body)
+          .then((dbUser) => res.json(dbUser))
+          .catch((err) => res.status(400).json(err));
+      },
+}
