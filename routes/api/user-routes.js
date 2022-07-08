15b68cb2948459a3789c488a8cec64ec8d476852
Tianchou Gong <tianchougong@gmail.com>
2022-07-08 06:06:58 -0400

    api routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+
+const {
+  getAllUsers,
+  createUser,
+  getUserById,
+  deleteUser,
+  updateUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/user-controller.js");
+
+
+router.route("/").get(getAllUsers).post(createUser);
+
+router.route("/:id").get(getUserById).delete(deleteUser).put(updateUser);
+
+router.route("/:id/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+module.exports = router;
