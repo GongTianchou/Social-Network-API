e4a2cdad1a67beef7e0570fead3bc653c3de250d
Tianchou Gong <tianchougong@gmail.com>
2022-07-08 06:31:38 -0400

    add thoughtSchema

--- models/Thought.js
+++ models/Thought.js
@@ -31,4 +31,43 @@ const reactionSchema = new Schema(
       getters: true,
     },
   }
+)
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    
+    username: {
+      type: String,
+      required: true,
+    },
+
+    reactions: [reactionSchema],
+
+    createAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
 );
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+module.exports = Thought;
