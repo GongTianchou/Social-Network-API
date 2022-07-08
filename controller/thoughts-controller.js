f5fc822b2e5557e8dd90d0cf25978dac0e2cf8de
Tianchou Gong <tianchougong@gmail.com>
2022-07-08 03:19:35 -0400

    create and update thought

--- controller/thoughts-controller.js
+++ controller/thoughts-controller.js
@@ -27,3 +27,35 @@ const thoughtController = {
         res.status(400).json(err);
       });
   },
+
+  createThoughts({ params, body }, res) {
+    Thoughts.create(body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { username: body.username },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "Did not find the User with this ID" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  updateThought({ params, body }, res) {
+    Thoughts.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "Did not find the thought with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+}
