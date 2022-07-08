d98176011e918a068b9f0cf377b3c52f4af28d1d
Tianchou Gong <tianchougong@gmail.com>
2022-07-08 02:02:38 -0400

    fix curly brackets error

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -2,49 +2,49 @@ const { User } = require("../models");
 
 const userController = {
 
-    getAllUsers(req, res) {
-      User.find()
-        .populate("thoughts")
-        .populate("friends")
-        .select("-__v")
-        .sort({ _id: -1 })
-        .then((dbUser) => res.json(dbUser))
-        .catch((err) => {
-          console.log(err);
-          res.status(400).json(err);
-        });
-    },
+  getAllUsers(req, res) {
+    User.find()
+      .populate("thoughts")
+      .populate("friends")
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbUser) => res.json(dbUser))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 
-    getUserById({ params }, res) {
-        User.findOne({ _id: params.id })
-          .populate({
-            path: "thoughts",
-            select: "-__v",
-          })
-          .populate({
-            path: "friends",
-            select: "-__v",
-          })
-          .select("-__v")
-          .then((dbUserData) => {
-            if (!dbUserData) {
-              res.status(404).json({ message: "Did not find the User with this ID" });
-              return;
-            }
-            res.json(dbUserData);
-          })
-          .catch((err) => {
-            console.log(err);
-            res.status(400).json(err);
-          });
-      },
-    
-      createUser({ body }, res) {
-        User.create(body)
-          .then((dbUser) => res.json(dbUser))
-          .catch((err) => res.status(400).json(err));
-      },
-}
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "Did not find the User with this ID" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUser) => res.json(dbUser))
+      .catch((err) => res.status(400).json(err));
+  },
+
 
 updateUser({ params, body }, res) {
   User.findOneAndUpdate({ _id: params.id }, body, {
@@ -98,6 +98,7 @@ deleteFriend({ params }, res) {
     })
     .catch((err) => res.status(400).json(err));
 },
-}
+
+};
 
 module.exports = userController;
